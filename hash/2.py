97c02831a9a372c09139d981b159cbbf338e7f18
twinklecherry <69239555+twinklecherry@users.noreply.github.com>
2021-07-31 22:37:13 +0900

    hash_2

--- hash/2.py
+++ hash/2.py
@@ -1,7 +1,18 @@
 #https://programmers.co.kr/learn/courses/30/lessons/42577
 def solution(phone_book):
     answer = True
+    hashMap = dict()
     phone_book.sort()
+
+    for phoneNo in phone_book : #숫자하나씩 딕셔너리에 담음
+        hashMap[phoneNo] = 1
+
+    for phoneNo in phone_book:
+        tmp = ''
+        for no in phoneNo: 
+            tmp += no
+            if tmp in hashMap and tmp != phoneNo: #딕셔너리 키로 같지만 전체 숫자가 다른경우
+                answer = False
     return answer
 
 print(solution(["119", "97674223", "1195524421"]))
